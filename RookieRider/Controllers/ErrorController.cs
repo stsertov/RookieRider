aabbe9ef96d371f4691b57f121457aac315d9810
Stoyan Sertov <stsrtoff@gmail.com>
2022-09-04 03:07:12 +0300

    Added custom 404 error page

---
+++ RookieRider/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿namespace RookieRider.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using RookieRider.Utility;
+
+    public class ErrorController : Controller
+    {
+        [Route("/Error/{statusCode}")]
+        public IActionResult StatusCodeViewBinder(int statusCode)
+        {
+            string route = string.Empty;
+            ViewData["ErrorMessage"] = $"Seems like you ran out of fuel... {Emoji.Sob}";
+            switch (statusCode)
+            {
+                case 404:
+                    route = "404";
+                    break;
+            }
+
+            return View(route);
+        }
+    }
+}
--- RookieRider/Controllers/HomeController.cs
+++ RookieRider/Controllers/HomeController.cs
@@ -20,7 +20,7 @@
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
